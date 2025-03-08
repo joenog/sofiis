2ea02c77c523fe9957b8c4441d1dc676ddd12419
joenog <joabnogueira@outlook.com>
2025-03-08 11:24:05 -0300

    created section fiis

--- routes.tsx
+++ routes.tsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
         element: <About />
       },
       {
-        path: '/notfound',
+        path: '*',
         element: <NotFound />
       },
       {
---
+++ src/services/api/api.js
@@ -0,0 +1,17 @@
+//API
+import axios from "axios";
+
+const API_URL = 'https://brapi.dev/api/quote/';
+
+async function fetchApi(codeFi) {
+  try {
+    const response = await axios.get(`${API_URL}${codeFi}?token=${'seRYmjyuvqd3rqixq2RLqS'}`);
+    console.log(response.data);
+    return response.data;
+  } catch (e) {
+    console.log(e)
+    throw error;
+  }
+}
+
+export default fetchApi
