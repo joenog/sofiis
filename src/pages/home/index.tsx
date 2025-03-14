7a2d4842460e72a6833ebdde659a4f87d4d73d60
joenog <joabnogueira@outlook.com>
2025-03-14 16:27:35 -0300

    enviroment variables created

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,10 +5,9 @@ import fiiCodes from "../../data/fiiCodes.ts";
 import Loading from "../../components/loading/index.tsx";
 
 export function Home() {
-
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<ApiProps[]>([]);
-  const [visibleCont, setVisiblecont] = useState(8);
+  const [visibleCont, setVisiblecont] = useState(6);
   const [error, setError] = useState<string | null>(null);
 
   //BUTTOM SHOW MORE
@@ -42,15 +41,22 @@ export function Home() {
 
   return(
     <>
-      <main className='w-full flex justify-center'>
-      {loading ? <Loading /> : (
+      <main className='flex flex-col items-center !px-4'>
+        <section className="md:w-5/10">
+          <h2>Favoritos</h2>
+          <div className="!p-4 bg-gray-800 rounded-xl">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate quaerat, accusamus quam tenetur temporibus aspernatur. Perspiciatis inventore ipsam praesentium fugiat quod consequatur qui possimus. Ipsa animi cumque adipisci delectus ducimus?</p>
+          </div>
+        </section>
 
-        <section className='flex !my-8 !mx-2 h-16 flex-col w-full md:w-5/10 gap-2'>
+      {loading ? <Loading /> : (
+        <section className='flex !my-8 flex-col w-full md:w-5/10 gap-2'>
+          <h2>Todos</h2>
           {
             data.slice(0, visibleCont).map((item, index) => (
               <div 
-                className="flex !p-4 justify-between rounded-2xl text-cyan-50 bg-gray-950" 
-                key={item.results[0]?.symbol || `index-${index}`}>
+              className="flex !p-4 justify-between rounded-2xl bg-gray-900" 
+              key={item.results[0]?.symbol || `index-${index}`}>
                 <span className="w-6">
                   <img src={item.results[0]?.logourl} alt="" />
                 </span>
@@ -62,10 +68,10 @@ export function Home() {
               </div>
             ))
           }
-          <span className="!pb-6 !m-6 flex justify-center items-center">
+          <span className="flex justify-end">
             <button 
               onClick={loadMore}
-              className="!px-8 !py-2 text-amber-50 bg-stone-700 rounded-2xl" type="button">
+              className="!px-4 !py-1 transition bg-gray-800 rounded-xl hover:bg-slate-600" type="button">
                 Mais
               </button>
           </span>
--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API_URL = 'https://brapi.dev/api/quote/';
+const apiKey = import.meta.env.VITE_API_KEY;
 
 async function fetchApi(codeFi: string) {
   try {
-    const response = await axios.get(`${API_URL}${codeFi}?token=${'seRYmjyuvqd3rqixq2RLqS'}`);
+    const response = await axios.get(`${API_URL}${codeFi}?token=${apiKey}`);
     console.log(response.data);
     return response.data;
   } catch (e) {
