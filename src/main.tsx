3b7a271fefb544b976a98dd19b04cb00ca796dfb
joenog <joabnogueira@outlook.com>
2025-03-29 20:46:44 -0300

    PWA created

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Workbox } from 'workbox-window';
+
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('/service-worker.js');
+  wb.register();
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
