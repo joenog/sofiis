fd97d26e37a972dd93e923b0fd9c542d41427d91
joenog <joabnogueira@outlook.com>
2025-04-02 18:49:53 -0300

    styles home fixed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -43,38 +43,40 @@ export function Home() {
             </div>
           </section>
 
-          <section className='flex !my-8 flex-col w-full md:w-6/10 gap-2'>
-            <h2>Favorites</h2>
-            {
-              data.slice(0, visibleCont).map((item, index) => (
-                <div 
-                className="flex !p-4 justify-between rounded-2xl bg-gray-900" 
-                key={item.results[0]?.symbol || `index-${index}`}>
-                  <span className="w-6">
-                    <img className="rounded-md" src={item.results[0]?.logourl} alt="" />
-                  </span>
-                  <span> {item.results[0]?.symbol}</span>
-                  <span>R$ {item.results[0]?.regularMarketPrice} </span>
-                { <span 
-                    className={item.results[0]?.regularMarketChangePercent < 0 ? 'text-red-600' : 'text-green-600'}> {item.results[0]?.regularMarketChangePercent.toFixed(2)}% 
-                  </span>}
-                </div>
-              ))
-            }
-            <span className="flex justify-end">
-              <button 
-                onClick={loadMore}
-                className="!px-4 !py-2 transition bg-gray-800 rounded-xl hover:bg-slate-600" type="button">
-                  <FaPlus size={13} />
-                </button>
-            </span>
-          </section>
-
-          <section className="md:w-6/10">
+           <section className="md:w-6/10">
             <h2>News</h2>
             <div className="!p-4 bg-gray-800 rounded-xl">
               { <p className="text-transparent">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo nihil corporis iste eos nobis harum tempore quo, quae animi minima, voluptas et iusto suscipit nulla esse maiores odit. Mollitia, temporibus.</p> }
             </div>
+          </section>
+
+          <section className='flex !my-2 flex-col w-full md:w-6/10 gap-2'>
+            <h2>Favorites</h2>
+            <div className="bg-gray-800 rounded-xl !p-2">
+              {
+                data.slice(0, visibleCont).map((item, index) => (
+                  <div
+                  className="flex !p-4 justify-between rounded-2xl bg-gray-900 !my-2"
+                  key={item.results[0]?.symbol || `index-${index}`}>
+                    <span className="w-6">
+                      <img className="rounded-md" src={item.results[0]?.logourl} alt="" />
+                    </span>
+                    <span> {item.results[0]?.symbol}</span>
+                    <span>R$ {item.results[0]?.regularMarketPrice} </span>
+                  { <span
+                      className={item.results[0]?.regularMarketChangePercent < 0 ? 'text-red-600' : 'text-green-600'}> {item.results[0]?.regularMarketChangePercent.toFixed(2)}%
+                    </span>}
+                  </div>
+                ))
+              }
+            </div>
+            <span className="flex justify-end">
+              <button 
+                onClick={loadMore}
+                className="!px-4 !py-2 transition bg-gray-800 rounded-xl hover:bg-slate-700" type="button">
+                  <FaPlus className="text-gray-500" size={13} />
+                </button>
+            </span>
           </section>
         </main>
       ) 
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -8,8 +8,8 @@ export function Profile() {
 
       <h1 className="!m-6"> </h1>
       <div className="flex flex-col h-96 justify-center items-center">
-        <img className="h-50 " src={profileImg} alt="" />
-        <h2 >soon...</h2>
+        <img className="h-50 opacity-26" src={profileImg} alt="" />
+        <p className="text-gray-700 text-xl">soon...</p>
       </div>
       <FloatingMenu />
     </main>
